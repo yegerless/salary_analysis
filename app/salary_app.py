585d345787a5f9cce04e05cdb85095042b4d9758
yegerless <Sergey.sys@mail.ru>
2024-04-07 17:13:58 +0300

    create parsing_data.py

--- app/salary_app.py
+++ app/salary_app.py
@@ -2,5 +2,9 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
+from app.Parsing_data import df_salary, df_inflation, df_salary_delta, df_real_salary
+
 st.header("Анализ динамики средней заработной платы в России в 2000-2023 годах")
+st.image(r'./app/static/money.jpg')
 st.write('Сравнение динамики средней заработной платы работников финансового сектора, здравоохранения и социальной поддержки, а также чиновников и военных')
+
